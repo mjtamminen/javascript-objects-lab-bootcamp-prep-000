d7ecd5a421f5019d7168a0b4d8d58b120d7be328
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-14 08:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, object, { key: [value] })
-  return object;
+  return Object.assign({}, object, { key: [value] })
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value
